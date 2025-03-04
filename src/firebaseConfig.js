0aaa23391a79ae14a8c4574a124b30b65fedafc3
diegogm13 <dg660262@gmail.com>
2025-03-04 17:41:34 -0600

    Primer commit

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAai0lpYyA-4Q8r_vBTTc6vOwk0aStSlgQ",
-    authDomain: "practica8-1df6f.firebaseapp.com",
-    projectId: "practica8-1df6f",
-    storageBucket: "practica8-1df6f.firebasestorage.app",
-    messagingSenderId: "669806938050",
-    appId: "1:669806938050:web:1a1711955404828e7df85b",
-    measurementId: "G-0SFNT272DN"
+  apiKey: "AIzaSyD7JnO31UTnmG_nD7b-knp-jqtfELdJ_7Y",
+  authDomain: "yara-70979.firebaseapp.com",
+  projectId: "yara-70979",
+  storageBucket: "yara-70979.firebasestorage.app",
+  messagingSenderId: "1043624290825",
+  appId: "1:1043624290825:web:6355641426304c5e21e6d8",
+  measurementId: "G-031N3X5CRQ"
   };
 
   const app = initializeApp(firebaseConfig);
